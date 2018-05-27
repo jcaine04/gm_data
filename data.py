ca1d3ce4e752801eb334cd75c31b484fc17e1741
John Caine <jcaine04@gmail.com>
2018-05-27 00:18:59 -0400

    message pulling working

--- data.py
+++ data.py
@@ -21,32 +21,34 @@ def get_session():
 
 class Group(Base):
     __tablename__ = 'groups'
-    id = Column(Integer, primary_key=True)
+    id = Column(String, primary_key=True)
     name = Column(String(75))
     description = Column(String(255))
 
 
 class Member(Base):
     __tablename__ = 'members'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer)
+    id = Column(String, primary_key=True)
+    user_id = Column(String)
     nickname = Column(String(100))
-    group_id = Column(Integer)
+    group_id = Column(String)
 
 
 class Message(Base):
     __tablename__ = 'messages'
-    id = Column(Integer, primary_key=True)
-    sender_id = Column(Integer)
-    user_id = Column(Integer)
+    id = Column(String, primary_key=True)
+    sender_id = Column(String)
+    user_id = Column(String)
     sender_name = Column(String(100))
     favorite_count = Column(Integer)
-    group_id = Column(Integer)
+    group_id = Column(String)
+    created_at = Column(Integer)
+    text = Column(Text)
 
 class Attachment(Base):
     __tablename__ = 'attachments'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    message_id = Column(Integer)
+    message_id = Column(String)
     attachment_num = Column(Integer)
     type = Column(String(25))
-    attachment_text = Column(Text)
+    attachment_url = Column(String)
--- main.py
+++ main.py
@@ -60,13 +60,108 @@ class GMGroup:
         self.store_members()
 
 
-class GMMember:
+class GMMessage:
 
-    def __init__(self):
-        self.endpoint = self.endpoint = f'https://api.groupme.com/v3/groups?token={GM_API_TOKEN}'
+    def __init__(self, group_id):
+        self.group_id = group_id
+        self.endpoint = f'https://api.groupme.com/v3/groups/{group_id}/messages?token={GM_API_TOKEN}&limit=100'
+        self.messages_data = []
+        self.last_message_id = None
+        self.db_session = get_session()
+
+    def pull_messages(self, before_id=None):
+        try:
+            if before_id:
+                logger.info(f"Before ID: {before_id}")
+                endpoint = f'{self.endpoint}&before_id={before_id}'
+            else:
+                endpoint = self.endpoint
+            logger.info(f"Endpoint: {endpoint}")
+            r = requests.get(endpoint)
+            status_code = r.status_code
+            if status_code == 304:
+                return status_code
+            self.messages_data = r.json()['response']['messages']
+        except Exception as e:
+            logger.error("Error pulling messages data")
+            logger.error(f"Group ID: {self.group_id}")
+            logger.error(f"After ID: {before_id}")
+            logger.error(self.endpoint)
+            logger.error(f"Request status code: {r.status_code}")
+            logger.error(f"Request response: {r.text}")
+            logger.error(e)
+            raise
+        if status_code == 200:
+            return status_code
+        else:
+            raise Exception(f"Bad status code: {r.status_code}, {r.text}")
+
+    def store_messages(self):
+        for message_dict in self.messages_data:
+            id = message_dict.get('id')
+            sender_id = message_dict.get('sender_id')
+            user_id = message_dict.get('user_id')
+            sender_name = message_dict.get('name')
+            favorite_count = len(message_dict.get('favorited_by'))
+            created_at = message_dict.get('created_at')
+            text = message_dict.get('text')
+
+            attachment_num = 1
+            for attachment_dict in message_dict.get('attachments'):
+                type = attachment_dict.get('type')
+                attachment_url = attachment_dict.get('url', None)
+                attachment = Attachment(message_id=id, attachment_num=attachment_num, type=type,
+                                        attachment_url=attachment_url)
+                try:
+                    self.db_session.add(attachment)
+                except Exception as e:
+                    logger.error("Unable to insert into database")
+                    raise
+                finally:
+                    self.db_session.close()
+                attachment_num += 1
+
+            message = Message()
+            message.id = id
+            message.sender_id = sender_id
+            message.user_id = user_id
+            message.sender_name = sender_name
+            message.favorite_count = favorite_count
+            message.group_id = self.group_id
+            message.created_at = created_at
+            message.text = text
+
+            try:
+                self.db_session.add(message)
+                self.db_session.commit()
+            except Exception as e:
+                logger.error("Unable to insert into database")
+                raise
+            finally:
+                self.db_session.close()
+            self.last_message_id = id
+
+    def message_runner(self):
+        i = 0
+        while True:
+            logger.info(f"Runnning iteration {i} of message puller")
+            status_code = self.pull_messages(self.last_message_id)
+            if status_code == 304:
+                break
+            self.store_messages()
+            i += 1
+
+
 
 
 
 if __name__ == '__main__':
-    g = GMGroup()
-    g.group_runner()
+    # create_database()
+
+    # get all groups and members
+    # g = GMGroup()
+    # g.group_runner()
+
+    # get all messages for one group
+    g_message = GMMessage(13989673)
+    g_message.message_runner()
